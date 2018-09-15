ce006992a64816cf6f3824ef9759307870f9118f
Amzad Chowdhury <amzad@amzadchowdhury.com>
2018-09-15 03:27:45 -0400

    Tip Calculator

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,9 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var billAmount: UITextField!
+    
+    @IBOutlet weak var partyLabel: UILabel!
+    @IBOutlet weak var partyCount: UISlider!
+    @IBOutlet weak var tipSegment: UISegmentedControl!
+    @IBOutlet weak var tipAmount: UILabel!
+    @IBOutlet weak var totalAmount: UILabel!
+    let step: Float = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+   
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +30,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.15, 0.18, 0.25]
+        
+        let roundedValue = round(partyCount.value / step) * step
+        partyCount.value = roundedValue
+        partyLabel.text = "Party Size: " + String(NSInteger(partyCount.value))
+        
+        let bill = Double(billAmount.text!) ?? 0
+        let tip = bill * tipPercentages[tipSegment.selectedSegmentIndex]
+        let total = (bill + tip) / Double(partyCount.value)
+        tipAmount.text = String(format: "$%.2f", tip)
+        totalAmount.text = String(format: "$%.2f", total)
+        
+        
+    }
 
 }
 
